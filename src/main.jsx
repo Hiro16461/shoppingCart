3eb3a211b9c943905f39fa274c7f5a133a437d5c
Hiro16461 <158548582+Hiro16461@users.noreply.github.com>
2026-03-13 07:13:31 +0900

    create browser router

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import './index.css';
+import App from './App.jsx';
+import Home from './components/Home.jsx';
+import Shop from './components/Shop.jsx';
+import Cart from './components/Cart.jsx';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: 'home', element: <Home /> },
+			{ path: 'shop', element: <Shop /> },
+			{ path: 'cart', element: <Cart /> },
+		],
+	},
+]);
+
+createRoot(document.getElementById('root')).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
+);
